4fbbc751500817c3c227759aead61feff47d64be
HELEN HARMAN <hlharman@bellsouth.net>
2020-10-05 01:30:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves (dwarf)
+  # code an argument here
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy", "Sleepy", "Sneezy", "Happy"].each_with_index do |dwarf|
+    puts  dwarf 
+end 
+end 
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
+
+summon_captain_planet (planateer_calls) # code an argument here
+  # Your code here 
+planateer_calls = ["earth, "wind", "fire", "water", "heart"]
+planateer_calls.map do |calls|
+yield|calls|
+
+end 
+
+
+summon_captain_planet(calls)
+calls_split " ".upcase.first + "! ".join 
+
+
+def long_planeteer_calls(calls)   # code an argument here
+  # Your code here
+  calls.map {calls.length > 4?}
+end 
+end 
+
+
+
+
+def find_the_cheese(ingredients)  # code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  cheese_types = ["cheddar", "gouda", "camembert"] 
+  ingredients.map |
+  ingredients.include? cheese_types 
+  return 
+end 
+end 
+
